0a5b675e9c13cf9585a49985a406d0217bb3e328
Igor Miguel <igormedeirosmiguel@gmail.com>
2024-05-17 19:24:47 +0000

    criação dos primeiros estilos

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import { HomeContainer } from "./Home.styles"
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <h1>
+        Home
+      </h1>
+    </HomeContainer>
+
+  )
+}
